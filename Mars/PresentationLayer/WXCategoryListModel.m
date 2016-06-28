b3082561503a5e554e9d173222e02d3238723eaf
wangxiao <wangxiao@ebupt.com>
2016-06-28 11:46:52 +0800

    测试推荐知识点和知识库打通

--- Mars/PresentationLayer/WXCategoryListModel.m
+++ Mars/PresentationLayer/WXCategoryListModel.m
@@ -9,5 +9,27 @@
 #import "WXCategoryListModel.h"
 
 @implementation WXCategoryListModel
+- (ZSBExerciseVideoModel *)zsbModel {
+    ZSBExerciseVideoModel *model = [[ZSBExerciseVideoModel alloc] init];
+    model.identifier = self.test_id;
+    model.title = self.title;
+    model.participateCount = [NSString stringWithFormat:@"%ld",(long)self.attend_count];
+    model.imageURL = self.video_image;
+    model.tag1 = self.tag1;
+    model.tag2 = self.tag2;
+    model.tag3 = self.tag3;
+    model.tag4 = self.tag4;
+    model.type = self.type;
+    return model;
+}
 
+//@property (nonatomic, strong) NSString *identifier;
+//@property (nonatomic, strong) NSString *title;
+//@property (nonatomic, strong) NSString *participateCount;
+//@property (nonatomic, strong) NSString *imageURL;
+//@property (nonatomic, strong) NSString *tag1;
+//@property (nonatomic, strong) NSString *tag2;
+//@property (nonatomic, strong) NSString *tag3;
+//@property (nonatomic, strong) NSString *tag4;
+//@property (nonatomic, strong) NSString *type;
 @end
