57555893b9084cb2e29f564fdb3589ab7286e1b9
wangxiao <wangxiao@ebupt.com>
2016-06-30 21:59:52 +0800

    排行榜隐藏播放按钮

--- Mars/PresentationLayer/RankCCell.m
+++ Mars/PresentationLayer/RankCCell.m
@@ -9,6 +9,7 @@
 #import "RankCCell.h"
 #import "WXRankModel.h"
 @interface RankCCell ()
+@property (weak, nonatomic) IBOutlet UIImageView *playView;
 @property (weak, nonatomic) IBOutlet UIImageView *titleView;
 @property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *iconView;
@@ -25,6 +26,12 @@
 
 - (void)setModel:(WXRankModel *)model {
     _model = model;
+    if ([model.type isEqualToString:@"video"]) {
+        self.playView.hidden = NO;
+    }
+    else {
+        self.playView.hidden = YES;
+    }
     self.scoreLabel.text = [NSString stringWithFormat:@"%@分",model.score];
     self.nameLabel.text = model.user_name;
     [self.iconView sd_setImageWithURL:[NSURL URLWithString:model.photo_url] placeholderImage:[UIImage imageNamed:@"placeholder"]];
