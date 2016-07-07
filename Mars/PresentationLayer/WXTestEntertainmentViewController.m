3d9883c056767d9c6f8950ba882b260c6cbb5005
wangxiao <wangxiao@ebupt.com>
2016-07-07 12:59:52 +0800

    完成趣味测试h5部分

--- Mars/PresentationLayer/WXTestEntertainmentViewController.m
+++ Mars/PresentationLayer/WXTestEntertainmentViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "WXTestEntertainmentViewController.h"
-
+#import "WXTestEntertainmentModel.h"
 @interface WXTestEntertainmentViewController ()
 @property (weak, nonatomic) IBOutlet UIButton *frontButton;
 @property (weak, nonatomic) IBOutlet UIButton *nextButton;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *widthConstraint;
-
+@property (nonatomic, copy) NSArray *modelLists;
+@property (nonatomic, assign) NSInteger index;
+@property (weak, nonatomic) IBOutlet UIWebView *myWebView;
 @end
 
 @implementation WXTestEntertainmentViewController
@@ -23,6 +25,8 @@
     self.navigationItem.title = @"趣味测试题";
     self.view.backgroundColor = [UIColor colorWithHexString:@"#F5F5F5"];
     [self configureButton];
+    self.index = 0;
+    [self loadData];
 }
 
 - (void)configureButton {
@@ -31,11 +35,17 @@
     self.frontButton.layer.masksToBounds = YES;
     [self.frontButton bk_whenTapped:^{
         NSLog(@"front");
+        if (self.index == 0) return;
+        self.index = self.index - 1;
+        [self loadUI];
     }];
     self.nextButton.layer.cornerRadius = self.nextButton.height/2;
     self.nextButton.layer.masksToBounds = YES;
     [self.nextButton bk_whenTapped:^{
         NSLog(@"next");
+        if (self.index == self.modelLists.count - 1) return;
+        self.index = self.index + 1;
+        [self loadUI];
     }];
     [self.view layoutIfNeeded];
 }
@@ -44,6 +54,39 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)loadData {
+    AFHTTPSessionManager *manager = [[NetworkManager sharedInstance] fetchSessionManager];
+    NSURL *url = [NSURL URLWithString:[URL_PREFIX stringByAppendingString:@"Test/Baiding/get_choice_list"]];
+    NSDictionary *parameters = nil;
+    [manager POST:url.absoluteString parameters:parameters progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"%@", responseObject);
+        if([responseObject[@"code"] isEqualToString:@"200"]) {
+            self.modelLists = [WXTestEntertainmentModel mj_objectArrayWithKeyValuesArray:responseObject[@"data"]];
+            [self loadUI];
+        }
+        else {
+            [SVProgressHUD showErrorWithStatus:responseObject[@"msg"]];
+            [self bk_performBlock:^(id obj) {
+                [SVProgressHUD dismiss];
+            } afterDelay:1.5];
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [SVProgressHUD showErrorWithStatus:@"网络异常"];
+        [self bk_performBlock:^(id obj) {
+            [SVProgressHUD dismiss];
+        } afterDelay:1.5];
+    }];
+}
+
+- (void)loadUI {
+    if (self.index < self.modelLists.count) {
+        WXTestEntertainmentModel *model = self.modelLists[self.index];
+        NSString *urlStr = model.url;
+        [self.myWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]]];
+    }
+}
+
 
 /*
 #pragma mark - Navigation
